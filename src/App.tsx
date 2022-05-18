51ffb1da5354e33f9405dadd6971df385328083d
engsekine <engsekine@gmail.com>
2022-05-18 12:14:25 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-console.log("./src/App.tsx");
+console.log('./src/App.tsx')
 
-import React from 'react';
+import React from 'react'
 
 export const App: React.VFC = () => {
   return <div>./src/App.tsx</div>
