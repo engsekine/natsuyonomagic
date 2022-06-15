62bf6db5ab8392ad61bde5a1c5f894a5479c59e8
engsekine <engsekine@gmail.com>
2022-06-15 10:12:03 +0900

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,14 @@
 const test = document.getElementById('test')
 const btn = document.getElementById('btn')
 btn.addEventListener('click', () => {
-  if (test.classList.contains('test2')) {
-    test.classList.remove('test2')
-  } else {
-    test.classList.add('test2')
-  }
+    if (test.classList.contains('test2')) {
+        test.classList.remove('test2')
+    } else {
+        test.classList.add('test2')
+    }
 })
 btn.addEventListener('click', () => {
-  test.classList.toggle('test3')
+    test.classList.toggle('test3')
 })
 
 //Grab divs with the class name 'match-height'
@@ -20,12 +20,12 @@ let heights = []
 
 //Create a loop that iterates through the getDivs variable and pushes the heights of the divs into an empty array
 for (let i = 0; i < arrayLength; i++) {
-  heights.push(getDivs[i].offsetHeight)
+    heights.push(getDivs[i].offsetHeight)
 }
 
 //Find the largest of the divs
 function getHighest() {
-  return Math.max(...heights)
+    return Math.max(...heights)
 }
 
 //Set a variable equal to the tallest div
@@ -33,56 +33,56 @@ let tallest = getHighest()
 
 //Iterate through getDivs and set all their height style equal to the tallest variable
 for (let i = 0; i < getDivs.length; i++) {
-  getDivs[i].style.height = tallest + 'px'
+    getDivs[i].style.height = tallest + 'px'
 }
 
 //マウスボタン押したとき発動
 window.addEventListener('mousedown', () => {
-  console.log('mousedown')
+    console.log('mousedown')
 })
 //マウスボタン離したとき発動
 window.addEventListener('mouseup', () => {
-  console.log('mouseup')
+    console.log('mouseup')
 })
 //マウス動かしたとき発動
 window.addEventListener('mousemove', () => {
-  console.log('mousemove')
+    console.log('mousemove')
 })
 //キーボード入力文字押したとき発動
 window.addEventListener('keypress', () => {
-  console.log('keypress')
+    console.log('keypress')
 })
 //キーボード押ているとき発動
 window.addEventListener('keydown', () => {
-  console.log('keydown')
+    console.log('keydown')
 })
 //キーボード離したとき発動
 window.addEventListener('keyup', () => {
-  console.log('keyup')
+    console.log('keyup')
 })
 //スクロールしたとき発動
 window.addEventListener('scroll', () => {
-  console.log('scroll')
+    console.log('scroll')
 })
 //全てのロードが完了したら発動
 window.addEventListener('load', () => {
-  console.log('load')
+    console.log('load')
 })
 //DOMが完成したら発動
 window.addEventListener('DOMContentLoaded', () => {
-  console.log('DOMContentLoaded')
+    console.log('DOMContentLoaded')
 })
 //formを変更して入力ボタン押したとき発動
 const form = document.forms.form
 form.change.addEventListener('change', () => {
-  console.log('change')
+    console.log('change')
 })
 //submitボタン押したとき発動
 const button = document.forms.form
 button.addEventListener('submit', () => {
-  console.log('submit')
+    console.log('submit')
 })
 //クリック時に発動
 button.addEventListener('click', () => {
-  console.log('click')
+    console.log('click')
 })
